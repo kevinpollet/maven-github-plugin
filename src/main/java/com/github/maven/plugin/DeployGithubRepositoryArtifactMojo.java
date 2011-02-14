871c68e8400b153a94807987a99c35de7955249f
Kevin Pollet <pollet.kevin@gmail.com>
2011-02-14 19:55:17 +0100

    Add a build failure exception and possibility to exclude files to upload if no artifacts are configured.

--- src/main/java/com/github/maven/plugin/DeployGithubRepositoryArtifactMojo.java
+++ src/main/java/com/github/maven/plugin/DeployGithubRepositoryArtifactMojo.java
@@ -24,6 +24,7 @@ import com.github.maven.plugin.client.exceptions.GithubException;
 import com.github.maven.plugin.client.exceptions.GithubRepositoryNotFoundException;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.DirectoryScanner;
 
@@ -36,6 +37,11 @@ import org.codehaus.plexus.util.DirectoryScanner;
  */
 //TODO add Proxy support
 public class DeployGithubRepositoryArtifactMojo extends AbstractGithubMojo {
+
+	/**
+	 * @parameter
+	 */
+	private String[] excludes;
 
 	/**
 	 * @parameter
@@ -55,7 +61,6 @@ public class DeployGithubRepositoryArtifactMojo extends AbstractGithubMojo {
 			//if no artifacts are configured deploy artifacts matching ${project.artifactId}-${project.version}*
 			if ( artifacts == null ) {
 				String outputDir = project.getBuild().getDirectory();
-
 				DirectoryScanner scanner = new DirectoryScanner();
 				scanner.setBasedir( outputDir );
 				scanner.setIncludes(
@@ -63,19 +68,26 @@ public class DeployGithubRepositoryArtifactMojo extends AbstractGithubMojo {
 								project.getArtifactId() + "-" + project.getVersion() + "*"
 						}
 				);
+				scanner.setExcludes( excludes );
 				scanner.scan();
 
 				String[] files = scanner.getIncludedFiles();
-				artifacts = new Artifact[files.length];
-				for ( int i = 0; i < files.length; i++ ) {
-					Artifact artifact = new Artifact();
-					artifact.setFile( new File( outputDir, files[i] ) );
-					artifact.setDescription( project.getDescription() );
-					artifact.setOverride( overrideExistingFile );
-					artifacts[i] = artifact;
+				if ( files.length == 0 ) {
+					throw new MojoFailureException( "No artifacts to upload into repository " + getRepository() );
+				}
+				else {
+					artifacts = new Artifact[files.length];
+					for ( int i = 0; i < files.length; i++ ) {
+						Artifact artifact = new Artifact();
+						artifact.setFile( new File( outputDir, files[i] ) );
+						artifact.setDescription( project.getDescription() );
+						artifact.setOverride( overrideExistingFile );
+						artifacts[i] = artifact;
+					}
 				}
 			}
 
+			//upload artifacts to configured github repository
 			uploadArtifacts( artifacts );
 
 		}
@@ -95,28 +107,26 @@ public class DeployGithubRepositoryArtifactMojo extends AbstractGithubMojo {
 	}
 
 	private void uploadArtifacts(Artifact... artifacts) throws MojoExecutionException {
+		final Log log = getLog();
 		final GithubClient githubClient = new GithubClient( getLogin(), getToken() );
 
-		getLog().info( "" );
+		log.info( "" );
 		for ( Artifact artifact : artifacts ) {
 			if ( artifact.getFile() == null ) {
 				throw new MojoExecutionException( "Missing <file> into artifact configuration " );
 			}
-
-			String artifactName = artifact.getFile().getName();
-
-			getLog().info( "Uploading " + artifact );
+			log.info( "Uploading " + artifact );
 			if ( artifact.getOverride() ) {
-				githubClient.replace( artifactName, artifact.getFile(), artifact.getDescription(), getRepository() );
+				githubClient.replace(
+						artifact.getFile().getName(), artifact.getFile(), artifact.getDescription(), getRepository()
+				);
 			}
 			else {
 				githubClient.upload( artifact.getFile(), artifact.getDescription(), getRepository() );
 			}
 		}
-		getLog().info( "" );
-
+		log.info( "" );
 	}
 
 }
-
 
